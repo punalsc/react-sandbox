cacb541df234031e6ca122c71b5928b156e52820
Punal Chotrani <punalsc@gmail.com>
2019-06-10 17:31:28 +0100

    Updated using the context API

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import Cards from './components/Cards/Cards';
 import 'materialize-css/dist/css/materialize.min.css';
 import './App.css';
 
+import { Provider, Consumer } from './context';
+
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
-				<div className='container'>
-					<Header branding='List of Super Heroes' />
-					<Cards />
+			<Provider>
+				<div className='App'>
+					<div className='container'>
+						<Consumer>
+							{value => {
+								const { header } = value;
+								return <Header branding={header} />;
+							}}
+							{/* <Header branding={header.header} /> */}
+						</Consumer>
+
+						<Cards />
+					</div>
 				</div>
-			</div>
+			</Provider>
 		);
 	}
 }
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card';
-import { superHeroData } from '../../Data';
+
+import { Consumer } from '../../context';
 
 class Cards extends Component {
-	state = {
-		superHeroData
-	};
-
 	render() {
-		const { superHeroData } = this.state;
 		return (
 			<div className='cardWrapper'>
-				{superHeroData.map((eachData, index) => (
-					<Card key={index} eachData={eachData} />
-				))}
+				<Consumer>
+					{value => {
+						const { superHeroData } = value;
+						return superHeroData.map((eachData, index) => (
+							<Card key={index} eachData={eachData} />
+						));
+					}}
+				</Consumer>
 			</div>
 		);
 	}
